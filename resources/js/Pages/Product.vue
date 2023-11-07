02e3495b45ccd7458ae3e437b09e4851b34fa05a
Idris Shingarov <fauther_i@mail.ru>
2023-11-07 19:51:50 +0300

    update adaptive

--- resources/js/Pages/Product.vue
+++ resources/js/Pages/Product.vue
@@ -67,14 +67,12 @@
                         </svg>
                         в корзине
                     </button>
-                    <Link :href="route('cart.index')" v-else>
-                        <button class="btn btn-transparent">
-                            В корзине
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                                <path d="M5 14L8.23309 16.4248C8.66178 16.7463 9.26772 16.6728 9.60705 16.2581L18 6" stroke="#222222" stroke-linecap="round"/>
-                            </svg>
-                        </button>
-                    </Link>
+                    <button class="btn btn-transparent"  v-else>
+                        В корзине
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                            <path d="M5 14L8.23309 16.4248C8.66178 16.7463 9.26772 16.6728 9.60705 16.2581L18 6" stroke="#222222" stroke-linecap="round"/>
+                        </svg>
+                    </button>
                 </div>
             </div>
         </div>
