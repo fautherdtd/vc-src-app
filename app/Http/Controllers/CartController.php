27a971283c9666928655680b3666a353fe6458b1
Idris Shingarov <fauther_i@mail.ru>
2023-11-10 22:44:46 +0300

    fixed location

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -61,7 +61,7 @@ class CartController extends Controller
                 'amount' => $data['order']['total_price'],
                 'description' => $data['order']['number'],
             ]);
-            return \redirect($result);
+            return Inertia::location($result);
         }
     }
 
