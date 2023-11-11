3c10396010faf05765cccf9c147d901e9930e00a
Idris Shingarov <fauther_i@mail.ru>
2023-11-11 18:58:03 +0300

    add empty favorites text

--- resources/js/Pages/Favorites.vue
+++ resources/js/Pages/Favorites.vue
@@ -8,13 +8,17 @@
                 :key="product.id"
                 :product="product"/>
         </div>
+        <div class="page-container" v-if="$page.props.share.favorites.totalQuantity === 0">
+            Вы пока ничего не добавили в избранное.
+            <Link :href="route('catalog')" style="color: #000; border-bottom: 1px dashed">Вернуться в в каталог.</Link>
+        </div>
     </AppLayout>
 </template>
 <script setup>
 import AppLayout from "@/Layouts/AppLayout.vue";
 import ProductCart from "@/Components/Products/ProductCart.vue";
 import BreadCrumbs from "@/Components/Common/BreadCrumbs.vue";
-
+import {Link, router} from "@inertiajs/vue3";
 const props = defineProps({
     favorites: Object
 })
