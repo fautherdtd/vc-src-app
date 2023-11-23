3cf7bedb1089c362b38ae8087ae58ab23265f921
Idris Shingarov <fauther_i@mail.ru>
2023-11-23 22:37:08 +0300

    fixed delivery

--- resources/js/Mixins/DeliveryPrice.js
+++ resources/js/Mixins/DeliveryPrice.js
@@ -4,11 +4,11 @@ const start = {
     latitude: 42.056954,
     longitude: 48.305317
 }
-const landingPrice = 100;
+const landingPrice = 50;
 
 const distancePrice = (end) => {
     let distance = Math.ceil(Math.ceil(haversine(start, end)) + + Math.floor(10));
-    return Math.ceil(landingPrice + (Math.ceil(distance * 50)));
+    return Math.ceil(landingPrice + (Math.ceil(distance * 30)));
 }
 
 export default distancePrice;
