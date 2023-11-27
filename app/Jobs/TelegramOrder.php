0689301f6c98dc3dd234d980d4e5d20af0481ee2
Idris Shingarov <fauther_i@mail.ru>
2023-11-27 14:27:17 +0300

    update

--- app/Jobs/TelegramOrder.php
+++ app/Jobs/TelegramOrder.php
@@ -30,16 +30,18 @@ class TelegramOrder implements ShouldQueue
     public function handle(): void
     {
         $order = Order::where('number', $this->numberID)->first();
-        $client = new Client([
-            'base_uri' => 'https://api.telegram.org/bot6411993510:AAEXUmjb3VeVrvV4vOMcbS5rqYyOpyla_Z4/',
-            'timeout'  => 2.0,
+        $ch = curl_init("https://api.telegram.org/bot6411993510:AAEXUmjb3VeVrvV4vOMcbS5rqYyOpyla_Z4/sendMessage?");
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, [
+            'chat_id' => -4040710684,
+            'text'  => 'Поступил заказ №'. $this->numberID .'. <a href="https://valscvetov.ru/admin/order/show/'. $order->id .'">Информация о заказе</a>',
+            'parse_mode' => 'html'
         ]);
-        $client->get('sendMessage', [
-            'query' => [
-                'chat_id' => -4040710684,
-                'text'  => 'Поступил заказ №'. $this->numberID .'. <a href="https://valscvetov.ru/admin/order/show/'. $order->id .'">Информация о заказе</a>',
-                'parse_mode' => 'html'
-            ]
-        ]);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ["Content-Type:multipart/form-data"]);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        $result = curl_exec($ch);
+        curl_close($ch);
     }
 }
