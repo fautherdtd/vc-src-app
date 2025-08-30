c38d874fef3d08fec32a6eb56780807c32b6681d
Idris Shingarov <i.shingarov@cryptoinfluence.app>
2025-08-30 15:55:23 +0300

    fix

--- app/Http/Controllers/Helpers/Images.php
+++ app/Http/Controllers/Helpers/Images.php
@@ -8,11 +8,14 @@ use Orchid\Attachment\Models\Attachment;
 trait Images
 {
     /**
-     * @param array $data
+     * @param Attachment|null $attachment
      * @return string
      */
-    public function getUrl(Attachment $attachment): string
+    public function getUrl(Attachment $attachment = null): string
     {
+        if (is_null($attachment)) {
+            return 'https://avatars.mds.yandex.net/get-tycoon/6283051/2a0000018bb8ee8143b8a38ebea0307ea3b0/priority-headline-logo-square';
+        }
         return Storage::disk('public')->url($attachment->path . $attachment->name . '.' . $attachment->extension);
     }
 }
